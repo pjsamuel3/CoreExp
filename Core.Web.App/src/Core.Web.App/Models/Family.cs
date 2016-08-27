e615f73db92925d9038d4b0d174895b278c2d5cb
Peter Samuel <pjsamuel3@hotmail.com>
2016-08-27 14:10:23 +0200

    Added some dummy models

---
+++ Core.Web.App/src/Core.Web.App/Models/Family.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Web.App.Models
+{
+    public class Family
+    {
+        public IEnumerable<Administrator> Administrators { get; set; }
+        public IEnumerable<Contributer> Contributers { get; set; }
+        public IEnumerable<Earner> Earners { get; set; }
+    }
+}
